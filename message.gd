e128f34821cd60332ade239502718464557a4538
Alexey Lukashevich <las900372@gmail.com>
2024-01-23 01:13:40 +0300

    Message
    Реализованы функции поиска самой длиной строки в сообщении, подсчёт числа строк сообщения, получение длины строки в пикселях при использовании требуемого шрифта

--- message.gd
+++ message.gd
@@ -26,15 +26,48 @@ func update_message() -> void:
 	else:
 		rect_min_size = MESSAGE_MIN_SIZE_EMPTY
 	
-	var lines_count: int = text.count('\n') + 1 # Число строк в сообщении
-	print(text) 
+	print(get_longest_text_line())
 
 	# Записываем текст и время в поле сообщения
 	$Text.bbcode_text = text + time_tags_start + time_formatted + time_tags_end
 
 
+# Получить длину строки, в пикселях
 func get_string_pixel_length(string: String) -> Vector2:
 	return message_font.get_string_size(string).x
+
+
+# Получить число строк в сообщении
+func get_lines_count() -> int:
+	return text.count('\n') + 1
+
+
+# Получить самую длинную строку в сообщении
+func get_longest_text_line() -> String:
+	if get_lines_count() == 1:
+		return text
+	
+	var longest_line := ""
+	var prev_line_break_pos := 0	# Номер предыдущего переноса
+	var next_line_break_pos := 0	# Номер следующего переноса
+	var substr_length := 0 			# Длина подстроки
+	var test_line: String			# Строка, которая проверяется
+	
+	for i in get_lines_count():
+		next_line_break_pos = text.find('\n', prev_line_break_pos) # Находим следующий перенос строки, \n
+		substr_length = next_line_break_pos - prev_line_break_pos  # Длина строки равна разнице между следующим и предыдущим переносами строки
+		
+		if substr_length < 0: # длина подстроки меньше нуля - строка последняя
+			test_line = text.substr(prev_line_break_pos) # строка последняя, считываем до конца
+		else:
+			test_line = text.substr(prev_line_break_pos, substr_length) # Возвращается строка, не включающая последний символ номером substr_length!
+		
+		if longest_line.length() < test_line.length():
+			longest_line = test_line
+			
+		prev_line_break_pos = next_line_break_pos + 1 	# на единицу больше, т.к. next_line_break_pos хотим искать мимо предыдущего prev_line_break_pos
+														# иначе при поиске будет находить сноску под номером prev_line_break_pos
+	return longest_line
 
 
 func set_text(value: String):
